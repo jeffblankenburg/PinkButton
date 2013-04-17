7b159d426436f0735f9596c6c18e4256b05ef327
Jeff Blankenburg <github@jeffblankenburg.com>
2013-04-16 22:57:24 -0400

    Initial Commit

---
+++ PinkButton/Home.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Navigation;
+using Microsoft.Phone.Controls;
+using Microsoft.Phone.Shell;
+using System.Windows.Media.Imaging;
+
+namespace PinkButton
+{
+    public partial class Home : PhoneApplicationPage
+    {
+        BitmapImage PinkButtonTapped;
+        BitmapImage PinkButtonNormal;
+        
+        public Home()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+            string imagepath = "Assets/Images/pinkbutton_tapped.png";
+            PinkButtonTapped = new BitmapImage(new Uri(imagepath, UriKind.Relative));
+            TheButton.Source = PinkButtonTapped;
+            imagepath = "Assets/Images/pinkbutton.png";
+            PinkButtonNormal = new BitmapImage(new Uri(imagepath, UriKind.Relative));
+            TheButton.Source = PinkButtonNormal;
+        }
+
+        private void PinkButton_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            
+            TheButton.Source = PinkButtonTapped;
+        }
+
+        private void PinkButton_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            TheButton.Source = PinkButtonNormal;
+        }
+    }
+}
---
+++ PinkButton/MainPage.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+
+namespace PinkButton
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void SkipButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            GoHome();
+        }
+
+        private void SaveButton_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            GoHome();
+        }
+
+        private void GoHome()
+        {
+            NavigationService.Navigate(new Uri("/Home.xaml", UriKind.Relative));
+        }
+    }
+}
