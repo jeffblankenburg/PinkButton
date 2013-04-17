9899ac74aa93f9950ec721653a6e55286da52b99
Jeff Blankenburg <github@jeffblankenburg.com>
2013-04-17 10:15:20 -0400

    Most pages done.

--- PinkButton/Home.xaml.cs
+++ PinkButton/Home.xaml.cs
@@ -15,10 +15,21 @@ namespace PinkButton
     {
         BitmapImage PinkButtonTapped;
         BitmapImage PinkButtonNormal;
+        BitmapImage UpdateTapped;
+        BitmapImage UpdateNormal;
         
         public Home()
         {
             InitializeComponent();
+            Loaded += Home_Loaded;
+        }
+
+        void Home_Loaded(object sender, RoutedEventArgs e)
+        {
+            TheButton.Source = PinkButtonTapped;
+            TheButton.Source = PinkButtonNormal;
+            UpdateButton.Source = UpdateTapped;
+            UpdateButton.Source = UpdateNormal;
         }
 
         protected override void OnNavigatedTo(NavigationEventArgs e)
@@ -26,21 +37,37 @@ namespace PinkButton
             base.OnNavigatedTo(e);
             string imagepath = "Assets/Images/pinkbutton_tapped.png";
             PinkButtonTapped = new BitmapImage(new Uri(imagepath, UriKind.Relative));
-            TheButton.Source = PinkButtonTapped;
+            
             imagepath = "Assets/Images/pinkbutton.png";
             PinkButtonNormal = new BitmapImage(new Uri(imagepath, UriKind.Relative));
-            TheButton.Source = PinkButtonNormal;
+
+            imagepath = "Assets/Images/button_clickheretoupdateyourinfo_tapped.png";
+            UpdateTapped = new BitmapImage(new Uri(imagepath, UriKind.Relative));
+            
+            imagepath = "Assets/Images/button_clickheretoupdateyourinfo.png";
+            UpdateNormal = new BitmapImage(new Uri(imagepath, UriKind.Relative));
+            
         }
 
         private void PinkButton_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
-            
             TheButton.Source = PinkButtonTapped;
         }
 
         private void PinkButton_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             TheButton.Source = PinkButtonNormal;
+        }
+
+        private void Update_MouseLeftButtonDown(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            UpdateButton.Source = UpdateTapped;
+        }
+
+        private void Update_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
+        {
+            UpdateButton.Source = UpdateNormal;
+            NavigationService.Navigate(new Uri("/MainPage.xaml", UriKind.Relative));
         }
     }
 }
--- PinkButton/MainPage.xaml.cs
+++ PinkButton/MainPage.xaml.cs
@@ -35,5 +35,85 @@ namespace PinkButton
         {
             NavigationService.Navigate(new Uri("/Home.xaml", UriKind.Relative));
         }
+
+        private void NameBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (NameBox.Text == "Name") NameBox.Text = "";
+        }
+
+        private void NameBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (NameBox.Text == "") NameBox.Text = "Name";
+        }
+
+        private void AddressBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (AddressBox.Text == "Address") AddressBox.Text = "";
+        }
+
+        private void AddressBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (AddressBox.Text == "") AddressBox.Text = "Address";
+        }
+
+        private void PhoneBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (PhoneBox.Text == "Mobile Phone #") PhoneBox.Text = "";
+        }
+
+        private void PhoneBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (PhoneBox.Text == "") PhoneBox.Text = "Mobile Phone #";
+        }
+
+        private void EmailBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (EmailBox.Text == "Email Address") EmailBox.Text = "";
+        }
+
+        private void EmailBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (EmailBox.Text == "") EmailBox.Text = "Email Address";
+        }
+
+        private void CarModelBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (CarModelBox.Text == "Make/Model of your car") CarModelBox.Text = "";
+        }
+
+        private void CarModelBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (CarModelBox.Text == "") CarModelBox.Text = "Make/Model of your car";
+        }
+
+        private void InsuranceBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (InsuranceBox.Text == "Your insurance company") InsuranceBox.Text = "";
+        }
+
+        private void InsuranceBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (InsuranceBox.Text == "") InsuranceBox.Text = "Your insurance company";
+        }
+
+        private void DeductibleBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (DeductibleBox.Text == "Deductible amount") DeductibleBox.Text = "";
+        }
+
+        private void DeductibleBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (DeductibleBox.Text == "") DeductibleBox.Text = "Deductible amount";
+        }
+
+        private void TextMessageBox_GotFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextMessageBox.Text == "Text message you'd like your emergency contact to receive") TextMessageBox.Text = "";
+        }
+
+        private void TextMessageBox_LostFocus(object sender, RoutedEventArgs e)
+        {
+            if (TextMessageBox.Text == "") TextMessageBox.Text = "Text message you'd like your emergency contact to receive";
+        }
     }
 }
